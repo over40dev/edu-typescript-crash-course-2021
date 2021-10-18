9614b8e5536c5eb4c243cfe31568d0cd9d718e3e
Cogentx <code@cogentx.com>
2021-10-18 14:12:40 -0230

    ENUM

--- dist/index.js
+++ dist/index.js
@@ -13,9 +13,49 @@ let employee = [
     [
         1, 'me'
     ],
-    [2, 'you'],
+    [2, 'you']
 ];
-// Union - either or
+// Unions - either or
 let pid = 22;
 pid = 22;
 pid = '22';
+// ENUM - constants list - default index 0
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+// ENUM - constants list - change index 0 and rest change to be next in numeric
+// sequence
+var Direction2;
+(function (Direction2) {
+    Direction2[Direction2["Up"] = 1] = "Up";
+    Direction2[Direction2["Down"] = 2] = "Down";
+    Direction2[Direction2["Left"] = 3] = "Left";
+    Direction2[Direction2["Right"] = 4] = "Right";
+})(Direction2 || (Direction2 = {}));
+// ENUM - constants list - can equate to string instead of index number
+var Direction3;
+(function (Direction3) {
+    Direction3["Up"] = "Up";
+    Direction3["Down"] = "Down";
+    Direction3["Left"] = "Left";
+    Direction3["Right"] = "Right";
+})(Direction3 || (Direction3 = {}));
+// Direction ENUM
+console.log(Direction.Up); // 0
+console.log(Direction.Down); // 1
+console.log(Direction.Left); // 2
+console.log(Direction.Right); // 3
+// Direction2 ENUM
+console.log(Direction2.Up); // 1
+console.log(Direction2.Down); // 2
+console.log(Direction2.Left); // 3
+console.log(Direction2.Right); // 4
+// Direction3 ENUM
+console.log(Direction3.Down); // Up
+console.log(Direction3.Up); // Down
+console.log(Direction3.Left); // Left
+console.log(Direction3.Right); // Right
