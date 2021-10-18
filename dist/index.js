31e6bd4ee8d95fd9d13c50ae0399013da62b052e
Cogentx <code@cogentx.com>
2021-10-18 13:24:33 -0230

    initial commit and basic types

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Me Inc.';
+let isPub = true;
+let x = 'Hello';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Basic Types
+let id : number = 5;
+let company : string = 'Me Inc.';
+let isPub : boolean = true;
+let x : any = 'Hello';
+
